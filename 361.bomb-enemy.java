e7e7bc7723acd330d0178b4ae8e5210b6a2d94ed
LHFY <38107178+lhfy1990@users.noreply.github.com>
2019-03-27 12:55:10 -0400

    03/27/2019 12:55

--- 361.bomb-enemy.java
+++ 361.bomb-enemy.java
@@ -1,10 +1,26 @@
-// Assumption:
-// Edge:
-// Algorithm:
-// Complexity:
-
+// Assumption: 1. all valid value
+// Edge: 1. zero size
+// Algorithm: Dynamic Programming
+// Complexity: O(mn)
+// keep two additional grid, for horizontal and vertical enemy count
 class Solution {
     public int maxKilledEnemies(char[][] grid) {
+        int height = grid.length;
+        if (height == 0) return 0;
+        int width = grid[0].length;
+        if (width == 0) return 0;
+        int[][] gh = new int[height][width];
+        int[][] gv = new int[height][width];
+        for(int i=0;i<height;i++) {
+            for (int j=0;j<width;j++) {
+                // loop and update both grids
+            }
+        }
+        for(int i=0;i<height;i++) {
+            for (int j=0;j<width;j++) {
+                // loop and get max value
+            }
+        }
         
     }
 }
